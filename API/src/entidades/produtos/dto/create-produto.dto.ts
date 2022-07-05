d81bef7f4745a8293f83821f5d19a45ca2aec8a5
NestorBrasileiro <nestor.brasileiro@ozmap.com.br>
2022-07-05 04:57:00 -0300

    refactor(produtos): separando o que é requerido no create e no update

--- API/src/entidades/produtos/dto/create-produto.dto.ts
+++ API/src/entidades/produtos/dto/create-produto.dto.ts
@@ -1,27 +1,21 @@
-import { IsString, Length, Min, Max, IsInt, IsNumber } from 'class-validator';
+import { IsString, IsInt, IsNumber } from 'class-validator';
 
 export class CreateProdutoDto {
   @IsInt()
   idCategoria: number;
 
   @IsString()
-  @Length(0, 30)
   codigo: string;
 
   @IsString()
-  @Length(0, 30)
   nome: string;
 
   @IsString()
-  @Length(0, 30)
   descricao: string;
 
   @IsNumber()
-  @Length(0, 30)
   valor: number;
 
   @IsInt()
-  @Min(0, { message: 'O numero deve ser 0(Inativo) ou 1(Ativo) ' })
-  @Max(1, { message: 'O numero deve ser 0(Inativo) ou 1(Ativo) ' })
   status: number;
 }
--- API/src/entidades/produtos/dto/update-produto.dto.ts
+++ API/src/entidades/produtos/dto/update-produto.dto.ts
@@ -1,4 +1,22 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { CreateProdutoDto } from './create-produto.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateProdutoDto extends PartialType(CreateProdutoDto) {}
+export class UpdateProdutoDto extends CreateProdutoDto {
+  @IsOptional()
+  idCategoria: number;
+
+  @IsOptional()
+  codigo: string;
+
+  @IsOptional()
+  nome: string;
+
+  @IsOptional()
+  descricao: string;
+
+  @IsOptional()
+  valor: number;
+
+  @IsOptional()
+  status: number;
+}
